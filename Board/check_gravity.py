4ffce7f0ce7d474dfd30b58075176760dcf85f10
Klautia08 <s55788@edu.campus02.at>
2025-02-08 18:02:19 +0100

    multiplayer added

--- Board/check_gravity.py
+++ Board/check_gravity.py
@@ -7,7 +7,13 @@ class Gravity(Board, ConvertCoordinates, EmptySlot):
         pass
 
     def gravity_check(self, new_coord):
-        """the function checks whether there is an empty space below the intended coordinate chosen by the user"""
+        """the function checks whether there is an empty space below the coordinate chosen by the user (input)"""
         super().__init__(new_coord)
         space = [None][None]
-        space[0] = new_coord[0]
+        space[0] = new_coord[0] + 1 #index 0 refers to the rows, adding one brings us one below the input
+        space[1] = new_coord[1]
+        if space[0] == 6:
+            return True
+        if EmptySlot.check_slot_space(space) == False:
+            return True
+        return False
